a0853cc582ee123c2921db90f390e5b293becd92
atmdboss <atmdboss@yahoo.com>
2020-06-28 09:36:57 +0300

    added genre scraping

--- backend/todo.md
+++ backend/todo.md
@@ -1,5 +1,4 @@
-1. Get genre for scraped movies.
-   The genres are not available on the DOM during scraping, so we need to figure out how to attach the genres to each movie.
+1. Match genres to year
 2. Generate statistic data and save that to database
 3. Create model for DB data
 4. Setup routes to get and set data
--- backend/utils/scraper.js
+++ backend/utils/scraper.js
@@ -5,6 +5,8 @@ const scrape = async (word) => {
 	const browser = await puppeteer.launch();
 	const URL = `http://www.quodb.com/search/${word}?p=1&titles_per_page=100&phrases_per_title=1`;
 
+	/* GET MOVIES BY PAGE FUNCTION DECLARATION START */
+
 	const moviesSinglePage = async (url) => {
 		try {
 			const page = await browser.newPage();
@@ -38,15 +40,41 @@ const scrape = async (word) => {
 			console.log(error.message);
 		}
 	};
-	const allMovies = await moviesSinglePage(URL);
+	/* GET MOVIES BY PAGE FUNCTION DECLARATION END */
 
-	fs.writeFile("demo.json", JSON.stringify(allMovies), (err) => {
+	/* GET GENRES FUNCTION DECLARATION START */
+
+	const getGenres = async (url) => {
+		const page = await browser.newPage();
+		await page.goto(url, { timeout: 0 });
+
+		const genres = await page.evaluate(() => {
+			return Array.from(document.querySelectorAll("#facets_list li"))
+				.filter((li) => li.querySelector("small"))
+				.map((li) => {
+					const genreName = li.querySelector("a").textContent;
+					const genreNumber = li.querySelector("small").textContent;
+
+					return {
+						genreName,
+						count: parseInt(genreNumber.substring(1, genreNumber.length - 1)),
+					};
+				});
+		});
+		return genres;
+	};
+	/* GET GENRES FUNCTION DECLARATION END */
+
+	const movies = await moviesSinglePage(URL);
+	const genres = await getGenres(URL);
+
+	fs.writeFile("demo.json", JSON.stringify({ movies, genres }), (err) => {
 		if (err) throw err;
 		console.log("The file has been saved!");
 	});
 
 	await browser.close();
-	// return {word,allMovies} && save to DB
+	// return {word,movies,genres:getGenres(URL)}
 };
 
 module.exports = scrape;
