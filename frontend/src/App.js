3af5a74429db9210296822b1c7c63406c3ffeaba
Shabnam Nikpour <snikpou2@uwo.ca>
2020-07-02 15:48:20 -0400

    new updates mostly CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,38 +3,38 @@ import LineChart from "./components/LineChart";
 import SearchBar from "./components/SearchBar";
 import fetchWord from "./helpers/fetchWord";
 import "./index.css";
+import "./App.css";
 
 function App() {
-	const [currentWord, setCurrentWord] = useState("");
-	const [word, setWord] = useState("");
+  const [currentWord, setCurrentWord] = useState("");
+  const [word, setWord] = useState("");
 
-	useEffect(() => {
-		if (currentWord !== "") {
-			fetchWord(currentWord).then((data) => {
-				setWord(data);
-			});
-		}
-	}, [currentWord]);
+  useEffect(() => {
+    if (currentWord !== "") {
+      fetchWord(currentWord).then((data) => {
+        setWord(data);
+      });
+    }
+  }, [currentWord]);
 
-	return (
-		<div className='App'>
-			<a
-				className='gitHub'
-				href='https://github.com/Hack-Your-Own-Holmes/word-trends'
-				target='_blank'
-				rel='noopener noreferrer'>
-				Link to Github
-			</a>
-			<header className='header'>Movie Word Trends</header>
+  return (
+    <div className="App">
+      <a
+        className="gitHub"
+        href="https://github.com/Hack-Your-Own-Holmes/word-trends"
+        target="_blank"
+        rel="noopener noreferrer"
+      ></a>
+      <header className="header">Movie Word Trends</header>
 
-			<SearchBar setCurrentWord={setCurrentWord} />
-			<div className='line-chart'>
-				{typeof word === "object" && (
-					<LineChart currentWord={currentWord} word={word} />
-				)}
-			</div>
-		</div>
-	);
+      <SearchBar setCurrentWord={setCurrentWord} />
+      <div className="line-chart">
+        {typeof word === "object" && (
+          <LineChart currentWord={currentWord} word={word} />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
