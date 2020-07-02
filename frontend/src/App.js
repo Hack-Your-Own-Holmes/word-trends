d2b11c478737d7227c52098f2cfd89505e6dee1a
atmdboss <atmdboss@yahoo.com>
2020-07-02 04:23:11 +0300

    minor refactor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,34 +5,36 @@ import fetchWord from "./helpers/fetchWord";
 import "./index.css";
 
 function App() {
-  const [currentWord, setCurrentWord] = useState("");
-  const [word, setWord] = useState("");
+	const [currentWord, setCurrentWord] = useState("");
+	const [word, setWord] = useState("");
 
-  useEffect(() => {
-    if (currentWord !== "") {
-      fetchWord(currentWord).then((data) => {
-        setWord(data);
-      });
-    }
-  }, [currentWord]);
+	useEffect(() => {
+		if (currentWord !== "") {
+			fetchWord(currentWord).then((data) => {
+				setWord(data);
+			});
+		}
+	}, [currentWord]);
 
-  return (
-    <div className="App">
-      <a
-        className="gitHub"
-        href="https://github.com/Hack-Your-Own-Holmes/word-trends"
-        target="_blank"
-      >
-        Link to Github
-      </a>
-      <header className="header">Movie Word Trends</header>
+	return (
+		<div className='App'>
+			<a
+				className='gitHub'
+				href='https://github.com/Hack-Your-Own-Holmes/word-trends'
+				target='_blank'
+				rel='noopener noreferrer'>
+				Link to Github
+			</a>
+			<header className='header'>Movie Word Trends</header>
 
-      <SearchBar setCurrentWord={setCurrentWord} />
-      <div className="line-chart">
-        {typeof word === "object" && <LineChart word={word} />}
-      </div>
-    </div>
-  );
+			<SearchBar setCurrentWord={setCurrentWord} />
+			<div className='line-chart'>
+				{typeof word === "object" && (
+					<LineChart currentWord={currentWord} word={word} />
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- frontend/src/components/LineChart.js
+++ frontend/src/components/LineChart.js
@@ -1,51 +1,49 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
-import generateColor from "../helpers/generateColor";
 
-const LineChart = ({ word }) => {
-  const years = Object.keys(word);
+const LineChart = ({ word, currentWord }) => {
+	const years = Object.keys(word);
 
-  const genres = word[years[0]]
-    ? Object.keys(word[years[0]].movies)
-    : ["Action"];
+	const genres = word[years[0]]
+		? Object.keys(word[years[0]].movies)
+		: ["Action"];
 
-  const datasets = genres.reduce((accumulator, currentGenre) => {
-    const color = generateColor();
-    const genreData = {
-      label: "Frequency Throughout The Years",
-      data: years.map((year) => {
-        return Object.values(word[year].movies[currentGenre]).reduce(
-          (acc, curr) => acc + curr,
-          0
-        );
-      }),
-      borderColor: color,
-      backgroundColor: color,
-      borderWidth: 1,
-    };
-    accumulator.push(genreData);
-    return accumulator;
-  }, []);
+	const datasets = genres.reduce((accumulator, currentGenre) => {
+		const genreData = {
+			label: currentWord,
+			data: years.map((year) => {
+				return Object.values(word[year].movies[currentGenre]).reduce(
+					(acc, curr) => acc + curr,
+					0
+				);
+			}),
+			borderColor: "rgb(35, 64, 153)",
+			backgroundColor: "rgba(35, 64, 153,0.3)",
+			borderWidth: 1,
+		};
+		accumulator.push(genreData);
+		return accumulator;
+	}, []);
 
-  const data = {
-    labels: years,
-    datasets,
-  };
+	const data = {
+		labels: years,
+		datasets,
+	};
 
-  const options = {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true,
-          },
-        },
-      ],
-    },
-    responsive: true,
-  };
+	const options = {
+		scales: {
+			yAxes: [
+				{
+					ticks: {
+						beginAtZero: true,
+					},
+				},
+			],
+		},
+		responsive: true,
+	};
 
-  return <Line data={data} options={options}></Line>;
+	return <Line data={data} options={options}></Line>;
 };
 
 export default LineChart;
